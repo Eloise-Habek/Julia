ff87d0c6944aa89afc90f433a291b82364c09595
Eloise-Habek <126615708+Eloise-Habek@users.noreply.github.com>
2023-03-31 20:35:27 +0200

    Add files via upload

---
+++ homework4.jl
@@ -0,0 +1,39 @@
+global m=70
+global cd=0.25
+global g=9.81
+
+function deriva(v)
+    dv=g-((cd/m)*v*abs(v))
+    return dv
+end
+
+function freeFallput(v0,t0,dt,tk)
+    deltaT = dt
+    vi = v0
+    s = 0
+    ti = t0
+    put = zeros(0)
+    br = zeros(0)
+    vrijeme = zeros(0)
+    while ti<tk
+        if ti+deltaT>tk
+            deltaT = tk-ti
+        end
+        append!(put,s)
+        append!(vrijeme,ti)
+        append!(br,vi)
+        dvdt = deriva(vi)
+        vi = vi + dvdt*deltaT
+        s = s-vi*deltaT
+        ti = ti+deltaT
+    end
+    return br,vrijeme,put
+end
+
+using Plots
+gr()
+b,vri, p = freeFallput(-40,0,2,20)
+
+
+plot(vri, p, label="Put ovisno o vremenu", legend = :bottomright)
+plot!(xlab="t [s]", ylab="s [m]")
