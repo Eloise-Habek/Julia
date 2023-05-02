bb32dc6d4ba8f4008319b1b70a34cf954a5e77a3
Eloise-Habek <126615708+Eloise-Habek@users.noreply.github.com>
2023-05-02 19:16:11 +0200

    Update SIR.jl

--- SIR.jl
+++ SIR.jl
@@ -12,8 +12,8 @@ end
 S = 4065253 #pronađena statistika o procjeni broja stanovnika u hrv 2019.
 I = 1 #pretpostavimo da zaraza u hrv počinje od jedne osobe
 R = 0
-k = 12
-b = 0.05
+k = 10 #pretpostavka je da su ljudi u kontaktu s otp 10 osoba na dan
+b = 0.6 #razni izvora govore da je gripa visoko zarazne u izravnom kontaktu
 t_oporavka = 7
 
 N = I + S + R
@@ -22,10 +22,10 @@ gamma = 1/t_oporavka
 
 p = (beta,gamma)
 u0 = [S;I;R]
-tspan = (0.0,100.0)
+tspan = (0.0,50.0)
 problem = ODEProblem(SIR,u0,tspan,p);
 sol = solve(problem)
 
-plot(sol,vars = (0,1),title = "SIR",label = "S")
-plot!(sol,vars = (0,2),label = "I")
-plot!(sol,vars = (0,3),label = "R")
+plot(sol,vars = (0,1),title = "SIR",label = "S",legend = :topright )
+plot!(sol,vars = (0,2),label = "I",legend = :topright)
+plot!(sol,vars = (0,3),label = "R",legend = :topright)
