69814cb750b53ecd7c8226b4f41933797de3c35c
Eloise-Habek <126615708+Eloise-Habek@users.noreply.github.com>
2023-05-02 19:03:56 +0200

    Add files via upload

---
+++ SIR.jl
@@ -0,0 +1,31 @@
+#Svaki podlozno postaje zarazen i onda oporavljen
+using DifferentialEquations
+using Plots
+
+function SIR(du,u,p,t)
+    beta,gamma = p
+    du[1] = -beta*u[1]*u[2]
+    du[2] = beta*u[1]*u[2]-gamma*u[2]
+    du[3] = gamma*u[2]
+end
+
+S = 4065253 #pronađena statistika o procjeni broja stanovnika u hrv 2019.
+I = 1 #pretpostavimo da zaraza u hrv počinje od jedne osobe
+R = 0
+k = 12
+b = 0.05
+t_oporavka = 10
+
+N = I + S + R
+beta = (k*b)/N
+gamma = 1/t_oporavka
+
+p = (beta,gamma)
+u0 = [S;I;R]
+tspan = (0.0,100.0)
+problem = ODEProblem(SIR,u0,tspan,p);
+sol = solve(problem)
+
+plot(sol,vars = (0,1),title = "SIR",label = "S")
+plot!(sol,vars = (0,2),label = "I")
+plot!(sol,vars = (0,3),label = "R")
